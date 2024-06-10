bc8ed3e35e783e69f9d196a10f829b6c83ecd9e5
nina <102603170+thaninamck@users.noreply.github.com>
2024-06-10 16:20:18 +0100

    test

--- app/src/main/java/com/example/easypark_api_front/model/Reservation.kt
+++ app/src/main/java/com/example/easypark_api_front/model/Reservation.kt
@@ -1,10 +1,13 @@
-package com.example.easypark.model
+package com.example.easypark_api_front.model
 
-class Reservation {
-    var id: Int = 0
-    var parking_name:String=""
-    var parking_id: Int = 0
-    var date:String=""
-    var parking_slot: String = ""
-    var parking_address: String = ""
-}
+data class Reservation (
+    var id: Int =0,
+    var parking_name:String,
+    var parking_id: Int ,
+    var date:String,
+    var parking_slot: String ,
+    var parking_address: String ,
+    var starthour:String,
+    var duration:String=""
+
+)
